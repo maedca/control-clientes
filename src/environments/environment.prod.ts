20e8ed52166164412691a53a622b9ce7c77ae7f5
Manuel Camacho <maedca@gmail.com>
2019-09-30 22:15:35 -0400

    setup firestore on modules.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
-};
+        production: true,
+        firestore: {
+            // Your web app's Firebase configuration
+
+            apiKey: "AIzaSyBLS4MyChTA4qgBvWUUldCHGY5vHzFP8hU",
+            authDomain: "control-clientes-24e7b.firebaseapp.com",
+            databaseURL: "https://control-clientes-24e7b.firebaseio.com",
+            projectId: "control-clientes-24e7b",
+            storageBucket: "control-clientes-24e7b.appspot.com",
+            messagingSenderId: "876458852091",
+            appId: "1:876458852091:web:9534a43020e9eddeed3a5e"
+
+        }
+    }
+;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+    production: false,
+    firestore: {
+        // Your web app's Firebase configuration
+
+        apiKey: "AIzaSyBLS4MyChTA4qgBvWUUldCHGY5vHzFP8hU",
+        authDomain: "control-clientes-24e7b.firebaseapp.com",
+        databaseURL: "https://control-clientes-24e7b.firebaseio.com",
+        projectId: "control-clientes-24e7b",
+        storageBucket: "control-clientes-24e7b.appspot.com",
+        messagingSenderId: "876458852091",
+        appId: "1:876458852091:web:9534a43020e9eddeed3a5e"
+
+    }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
