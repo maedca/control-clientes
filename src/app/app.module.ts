f984ebcbba2fdecc6470a91df96e2ebc41458f32
Manuel Camacho <maedca@gmail.com>
2019-09-30 20:08:23 -0400

    creacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
