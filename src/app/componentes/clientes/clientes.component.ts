3d3213389201d0423f7e738802df51724daffdc1
Manuel Camacho <maedca@gmail.com>
2019-10-01 20:19:05 -0400

    validando modal

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,35 +1,53 @@
 import {Component, OnInit} from '@angular/core';
-import {ClienteService} from "../../servicios/cliente.service";
-import {ClienteModel} from "../../modelo/cliente.model";
+import {ClienteService} from '../../servicios/cliente.service';
+import {ClienteModel} from '../../modelo/cliente.model';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
-    selector: 'app-clientes',
-    templateUrl: './clientes.component.html',
-    styleUrls: ['./clientes.component.css']
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-    clientes: ClienteModel[];
+  clientes: ClienteModel[];
+  cliente: ClienteModel = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
 
-    constructor(private clientesService: ClienteService) {
+  };
+
+  constructor(private clientesService: ClienteService, private flashMessages: FlashMessagesService) {
+  }
+
+  ngOnInit() {
+    this.clientesService.getClientes().subscribe(clientes => {
+        this.clientes = clientes;
+      }
+    )
+    ;
+  }
+
+  getSaldoTotal() {
+    let saldoTotal = 0;
+    if (this.clientes) {
+      this.clientes.forEach(cliente => {
+        saldoTotal += cliente.saldo;
+      });
     }
+    return saldoTotal;
 
-    ngOnInit() {
-        this.clientesService.getClientes().subscribe(clientes => {
-                this.clientes = clientes;
-            }
-        )
-        ;
+  }
+
+  agregar({value, valid}: { value: ClienteModel, valid: boolean }) {
+    if (!valid) {
+        this.flashMessages.show('Llena el formulario correctamente', {
+          cssClass: 'alert-danger',
+          timeout: 4000,
+
+        });
     }
-
-    getSaldoTotal() {
-        let saldoTotal: number = 0;
-        if (this.clientes) {
-            this.clientes.forEach(cliente => {
-                    saldoTotal += cliente.saldo;
-            });
-        }
-        return saldoTotal;
-
-    }
+  }
 
 }
